66e55570fcad0fb01c08603fd9bf1c12158e6d59
jojunsoo <junsoo850@naver.com>
2024-01-23 13:52:23 +0900

    Feat:common컬러설정완료

--- apps/sfaclog/src/app/page.tsx
+++ apps/sfaclog/src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
-
+import { Common } from "@/styles/common";
 export default function Home() {
   return (
-    <div>홈페이지</div>
+    <div>
+      홈페이지
+      <p>
+        테스트
+      </p>
+    </div>
   );
 }
