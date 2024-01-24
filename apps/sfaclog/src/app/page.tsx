6091466c567e66ccf5f235b8639b8e8ab03d9ac4
jojunsoo <junsoo850@naver.com>
2024-01-24 17:50:35 +0900

    Feat:Common스타일 수정

--- apps/sfaclog/src/app/page.tsx
+++ apps/sfaclog/src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
 import { Common } from "@/styles/common";
 
 export default function Home() {
   return (
     <div>
       홈페이지
-      <p className={`text-${Common.font.size.xxl}`}>테스트</p>
+      <p className={`text-${Common.colors.primary[70]} ${Common.font.size.xxl}`}>테스트</p>
     </div>
     
   );
 }
+
