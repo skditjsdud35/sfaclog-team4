abaf775871887a52ff4fbfffeb93880e3365f680
jojunsoo <junsoo850@naver.com>
2024-01-25 13:21:12 +0900

    Feat:common파일 수정

--- apps/sfaclog/src/app/page.tsx
+++ apps/sfaclog/src/app/page.tsx
@@ -4,9 +4,7 @@ export default function Home() {
   return (
     <div>
       홈페이지
-      <p className={`text-${Common.colors.primary[70]} ${Common.font.size.xxl}`}>
-        테스트
-      </p>
+      <p className={`text-${Common.colors.primary[70]} ${Common.font.size.xxl}`}>테스트</p>
     </div>
     
   );
