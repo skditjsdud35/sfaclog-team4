e9f1d3e9aa3af589ec8f419c3944639b72f59bd1
jojunsoo <junsoo850@naver.com>
2024-01-29 14:23:02 +0900

    Feat:api셋팅

--- apps/sfaclog/src/utils/api.ts
+++ apps/sfaclog/src/utils/api.ts
@@ -1,10 +1,11 @@
 import pb from "./pocketbase"
 import { userTypes } from "@/types/user";
 
-export const reqSignUp = (data: userTypes) =>
+//회원가입
+export const SignUp = (data: userTypes) =>
     pb.collection("users").create(data);
-
-export const reqLogin = (email: string, password: string) =>
+//로그인
+export const Login = (email: string, password: string) =>
     pb.collection("users").authWithPassword(email, password);
-
-export const reqGetToken = () => pb.authStore.token;
+//토큰
+export const GetToken = () => pb.authStore.token;
---
+++ apps/sfaclog/src/utils/junsoo.ts
@@ -0,0 +1,4 @@
+import pb from "./pocketbase"
+
+export const socialLogin = () =>
+
