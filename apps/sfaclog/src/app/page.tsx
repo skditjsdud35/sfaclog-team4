521e51632f4fd3b6d934b6ddecbf3e1c764ad6ed
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2024-01-29 17:51:35 +0900

    Feat: . 메세지보내기, 단일메세지확인 , 유저전체메세지확인 완성

--- apps/sfaclog/src/app/page.tsx
+++ apps/sfaclog/src/app/page.tsx
@@ -1,12 +1,54 @@
-import { Common } from "@/styles/common";
+"use client";
+import {
+  readAllMessage,
+  readSingleMessage,
+  sendMessage,
+} from "@/utils/jaehyun";
 
 export default function Home() {
+  const testData = {
+    sender: "cw1q1790mj13h9f",
+    receiver: "y53u78yj042nd15",
+    message: "테스트입니다.",
+    is_read: false,
+  };
+  // 메세지 보내기 테스트
+  const onSendMessageClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    sendMessage(testData);
+  };
+  // 전체 메세지 읽기 테스트
+  const onReadAllMessageClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+
+    readAllMessage("y53u78yj042nd15");
+  };
+
+  // 단일 메세지 읽기 테스트
+
+  const onReadMessageClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+
+    readSingleMessage("gxs2jj21ttt35k5");
+  };
+
   return (
     <div>
-      홈페이지
-      <p className={`text-${Common.colors.primary[70]} ${Common.font.size.xxl}`}>테스트</p>
+      <h1>테스트 페이지</h1>
+      <div className="p-5 bg-blue-500 ">
+        <button onClick={onSendMessageClick}>메세지 보내기 테스트 버튼</button>
+      </div>
+      <div className="p-5 bg-blue-400 ">
+        <button onClick={onReadAllMessageClick}>
+          현재 유저 메세지 확인 버튼
+        </button>
+      </div>
+      <div className="p-5 bg-blue-300 ">
+        <button onClick={onReadMessageClick}>단일 메세지 읽기</button>
+      </div>
     </div>
-    
   );
 }
-
--- apps/sfaclog/src/utils/jaehyun.ts
+++ apps/sfaclog/src/utils/jaehyun.ts
@@ -1,1 +1,51 @@
 // USER , LOG , MESSAGE
+import pb from "./pocketbase";
+
+// 메세지 보내기
+
+interface MessageData {
+  sender: string;
+  receiver: string;
+  message: string;
+  is_read: boolean;
+}
+
+export async function sendMessage(MessageData: MessageData) {
+  const data = {
+    ...MessageData,
+  };
+
+  try {
+    const record = await pb.collection("message").create(data);
+    console.log("메세지 보내기 성공 ", record);
+    return record;
+  } catch (error) {
+    console.error("메세지를 보내지못했습니다.", error);
+  }
+}
+
+// 전체 메세지 읽어오기
+export const readAllMessage = async (userId: string) => {
+  try {
+    const res = await pb.collection("message").getList(1, 50, {
+      filter: `receiver = "${userId}"`,
+    });
+    console.log(res);
+  } catch (err) {
+    console.error("메세지 를 읽어오지못했습니다. ", err);
+  }
+};
+
+// 단일 메세지 읽기
+export const readSingleMessage = async (messageId: string) => {
+  try {
+    const res = await pb.collection("message").getList(1, 50, {
+      filter: `id = "${messageId}"`, 
+    });
+    console.log(res);
+  } catch (err) {
+    console.error("메세지 를 읽어오지못했습니다. ", err);
+  }
+};
+
+// 단일 메세지 삭제 
