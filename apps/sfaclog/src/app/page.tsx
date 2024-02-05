5bf31b469c2f8637bebaa6cca195ffdb4d236f7f
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2024-02-05 16:54:09 +0900

    Feat: tailwindcss.config.js 플러그인 추가로 자동완성, 디자인시스템 theme 로 다시적용

--- apps/sfaclog/src/app/page.tsx
+++ apps/sfaclog/src/app/page.tsx
@@ -1,9 +1,11 @@
-import TestJaehyun from "@/test/test-jaehyun";
+import { Button } from "../../../../packages/ui";
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      <TestJaehyun />
+    <div className={`bg-black} h-screen`}>
+      {/* <TestJaehyun /> */}
+      <Button.v1 label="테스트" />
+      <div className="Header_B_36">dd</div>
     </div>
   );
 }
--- packages/tailwindcss/tailwind.config.js
+++ packages/tailwindcss/tailwind.config.js
@@ -11,11 +11,73 @@ module.exports = {
     "content/**/*.{md,mdx}",
   ],
   theme: {
+    screens: {
+      pc: "1440px",
+      tablet: "1023px",
+      mobile: "767px",
+    },
     extend: {
-      colors: {},
+      colors: {
+        primary: {
+          "primary-100": "#0059FF",
+          "primary-90": "#196AFF",
+          "primary-80": "#337AFF",
+          "primary-70": "#4C8BFF",
+          "primary-60": "#669BFF",
+          "primary-50": "#7FACFF",
+          "primary-40": "#99BDFF",
+          "primary-30": "#B2CDFF",
+          "primary-20": "#CCDEFF",
+          "primary-10": "#E5EEFF",
+          "primary-5": "#F5F8FF",
+          "primary-white": "#FFFFFF",
+        },
+        neutral: {
+          "neutral-100": "#030303",
+          "neutral-90": "#1A1A1A",
+          "neutral-80": "#333333",
+          "neutral-70": "#4D4D4D",
+          "neutral-60": "#666666",
+          "neutral-50": "#808080",
+          "neutral-40": "#999999",
+          "neutral-30": "#B3B3B3",
+          "neutral-20": "#CCCCCC",
+          "neutral-10": "#E6E6E6",
+          "neutral-5": "#F3F3F3",
+          "neutral-white": "#FFFFFF",
+        },
+        stroke: {
+          "stroke-blue": "#99BDFF",
+          "stroke-10": "#E6E6E6",
+          "stroke-5": "#F3F3F3",
+        },
+        background: {
+          "background-100": "#666666",
+          "background-50": "#666666",
+          "background-5": "#F8F8F9",
+          "background-blue": "#F5F8FF",
+        },
+        system: {
+          "system-warning": "#FF0000",
+          "system-success": "#07A320",
+        },
+      },
+      fontWeight: {
+        bold: "700",
+        medium: "500",
+        regular: "400",
+      },
     },
   },
   plugins: [
+    ({ addUtilities }) => {
+      addUtilities({
+        ".Header_B_36": {
+          width: "300px",
+          backgroundColor: "red",
+        },
+      });
+    },
     // require("prettier-plugin-tailwindcss"  )
   ],
 };
