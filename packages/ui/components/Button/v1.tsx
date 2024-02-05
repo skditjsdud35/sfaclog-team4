969d4006ea04ee1b3e6e45e0f35a92ef8c3e7ba3
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2024-02-05 21:24:29 +0900

    tailwindcss , storybook 오류 해결 및 테스트 완료

--- packages/ui/components/Button/v1.tsx
+++ packages/ui/components/Button/v1.tsx
@@ -1,33 +1,54 @@
-import { Common } from "../../styles/common";
-
 export interface ButtonProps {
   onClick?: () => void;
   label?: string;
-  backgroundColor?: string;
-  color?: string;
-  size?: "s" | "r" | "l";
+  type?: "button" | "submit" | "reset";
+  size?: "Large" | "Regular" | "Small";
+  color?: "Blue" | "Gray" | "Line";
 }
 
 function Button({
   label,
-  size,
-  backgroundColor = "primary-100",
-  color = "primary-white",
+  size = "Large",
+  color = "Blue",
+  type = "button",
   onClick,
 }: ButtonProps) {
-  let dimensions = `w-[103px] h-[52px]`;
-  if (size === "s") dimensions = "w-[62px] h-[30px]";
-  if (size === "r") dimensions = "w-[92px] h-[44px]";
-  const bgColor = `bg-${backgroundColor}`;
-  const txtColor = `text-${color}`;
-  console.log(bgColor);
+  const className = {
+    getColor: () => {
+      switch (color) {
+        case "Blue": {
+          return "bg-primary-100 text-primary-white";
+        }
+        case "Gray": {
+          return "bg-neutral-5 text-neutral-90";
+        }
+        case "Line": {
+          return "bg-primary-white text-neutral-60 border-[1px] border-stroke-10";
+        }
+      }
+    },
+    getSize: () => {
+      switch (size) {
+        case "Large": {
+          return "w-[103px] h-[52px]";
+        }
+        case "Regular": {
+          return "w-[92px] h-[44px]";
+        }
+        case "Small": {
+          return "w-[62px] h-[30px]";
+        }
+      }
+    },
+  };
+
   return (
-    <button className={`${bgColor} ${txtColor} ${dimensions}  `}>
+    <button
+      className={` ${className.getColor()} ${className.getSize()} rounded-xl `}
+    >
       {label}
     </button>
   );
 }
 
 export default Button;
-
-// 싲가
