fdb112081efa85ded44cc14f1b7e8374c7662511
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2024-02-06 13:14:59 +0900

    tailwindcss 클래스 폰트사이즈 수정

--- apps/sfaclog/src/app/page.tsx
+++ apps/sfaclog/src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Button } from "../../../../packages/ui";
 
 export default function Home() {
-
   return (
     <div className={`bg-black} h-screen`}>
       {/* <TestJaehyun /> */}
       <Button.v1 label="테스트" />
-      <div className="">dd</div>
+      <div className="Header_B_36">dd</div>
     </div>
   );
 }
-
